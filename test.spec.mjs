00dd69596e279885184321793045edc5b47640ea
Roland Broekema <broekema41@gmail.com>
2024-08-29 08:17:46 +0200

    itterator support and a test suite update according to latst keyv testing lib. (#21)
    * chore(ROLAND-001): updated to be green on latest test suite tslinted and more code readabillity
    
    * feature(ROLAND-002): adding itterator support
    
    * chore(ROLAND-003): cleaning up for pull request
    
    * chore(ROLAND-004): Exporting constructor to stay backwards compattible
    
    * fix(ROLAND-005): preventing overwrites of defaultconfig

---
+++ test.spec.mjs
@@ -0,0 +1,10 @@
+import Keyv from 'keyv';
+import KeyvStore from './lib/index.js';
+import keyvTestSuite, {keyvIteratorTests} from '@keyv/test-suite';
+import * as test from 'vitest';
+
+const store = () => new KeyvStore({
+  filename: `./node_modules/.cache/test-save-${Math.random().toString(36).slice(2)}.json`,
+});
+keyvTestSuite(test, Keyv, store);
+keyvIteratorTests(test, Keyv, store);
